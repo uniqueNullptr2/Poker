167b5d1042103158c65a796d1d67dd5a0c3bfb4d
uniqueNullptr2 <philipp.classen@protonmail.com>
2021-08-10 17:46:51 +0200

    maven project setup

---
+++ poker/src/main/java/unique/nullptr/poker/App.java
@@ -0,0 +1,13 @@
+package unique.nullptr.poker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
